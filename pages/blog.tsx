56d5317c3e06b661615dc8bacf393449d9a4780c
Fafly <louislebeke@gmail.com>
2019-10-08 19:35:06 +0200

    [Feature]: :sparkle: Fetch all the blog posts.

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,14 +2,31 @@ import React, { PureComponent } from 'react';
 
 import Layout from '../components/Layout';
 
+const URL_GET_POSTS = 'https://upply-interview.herokuapp.com/';
+
 class Blog extends PureComponent {
-    render() {
-        return (
-            <Layout>
-                <h1>Blog page</h1>
-            </Layout>
-        );
-    }
+  state = {
+    // Posts to display
+    posts: []
+  }
+
+  componentDidMount(): void {
+    this.getPosts();
+  }
+
+  getPosts = () => {
+    fetch(URL_GET_POSTS)
+      .then(response => response.json())
+      .then(posts => this.setState({ posts }));
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Blog page</h1>
+      </Layout>
+    );
+  }
 }
 
 export default Blog;
